8d5f29df20fa93ac77386ef3759739a3cab490e0
Sean Greenbaum <sean.greenbaum@microsoft.com>
2021-10-05 15:28:43 -0400

    updated

--- AD Schema changes by date.ps1
+++ AD Schema changes by date.ps1
@@ -2,8 +2,8 @@
 
 $schema = Get-ADObject -SearchBase ((Get-ADRootDSE).SchemaNamingContext) -SearchScope OneLevel -Filter * -Property objectClass, name, whenChanged,whenCreated | Select-Object objectClass, name, whenCreated, whenChanged, @{name="event";expression={($_.whenCreated).Date.ToShortDateString()}} | Sort-Object whenCreated
 
-"`nDetails of schema objects changed by date:" 
+Write-Host "`nDetails of schema objects changed by date:" 
 $schema | Format-Table objectClass, name, whenCreated, whenChanged -GroupBy event -AutoSize
 
-"`nCount of schema objects changed by date:" 
+Write-Host "`nCount of schema objects changed by date:" 
 $schema | Group-Object event | Format-Table Count, Name, Group –AutoSize 
